3c129cc5269488983688f37f7b24d3cb2e9c156f
Adam Sherman <adam.r.sherman@gmail.com>
2015-08-16 18:39:11 -0400

    Version bump to 0.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     author='Adam Sherman',
     author_email='adam.r.sherman@gmail.com',
-    description='Simple wrapper around TradeMonster/Optionhouses\' XML based API',
+    description='A simple to use wrapper around TradeMonster/Optionhouses\' XML based API',
     packages = find_packages(),
     install_requires = [ 'ggplot >= 0.6.5','pandas >= 0.13.1', 'xmltodict >= 0.9.2', 'simple-crypt >= 4.1.0']
 )
